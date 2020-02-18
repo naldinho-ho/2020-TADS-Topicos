03fb431f09618970e6d727b8fabd0de6667498f2
Aluno <naldinho-ho@hotmail.com>
2020-02-17 22:28:19 -0300

    sistema

---
+++ Progressao/paginas/views.py
@@ -0,0 +1,20 @@
+
+# Importar o TemplateView para criar páginas simples
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+
+# A classe PaginaInicial "extends" TemplateView
+class PaginaInicial(TemplateView):
+    # Toda classe filha do TemplateView precisa do
+    # atributo abaixo para definir um template a ser renderizado
+    template_name = 'paginas/index.html'
+
+
+class SobreView(TemplateView):
+    template_name = 'pagina/sobre.html'
+
+
+class AjudaView(TemplateView):
+    template_name = 'pagina/ajuda.html'
